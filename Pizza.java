dd6e2dd464eb0d6b06b4978c761d5701f3bf17a5
dtimokhi <dtimokhin@gmail.com>
2018-04-19 17:01:46 -0700

    Final Updates

---
+++ Pizza.java
@@ -0,0 +1,10 @@
+public class Pizza{
+	static int t = 0;
+	public static void main(String[] args) {
+
+	}
+	public int getCount() {
+		t++;
+		return t;
+	}
+}
---
+++ Tala.java
@@ -0,0 +1,8 @@
+public class Tala{
+	public static void main(String[] args){
+		Pizza test = new Pizza();
+		System.out.println(test.getCount());
+		Pizza test2 = new Pizza();
+		System.out.println(test2.getCount());
+	}
+}
