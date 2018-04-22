68b8b43dc02fa293d31c6a55b08708d6948e6e74
dtimokhi <dtimokhin@gmail.com>
2018-04-22 14:28:46 -0700

    removed certain files

--- Tala.java
+++
@@ -1,8 +0,0 @@
-public class Tala{
-	public static void main(String[] args){
-		Pizza test = new Pizza();
-		System.out.println(test.getCount());
-		Pizza test2 = new Pizza();
-		System.out.println(test2.getCount());
-	}
-}
